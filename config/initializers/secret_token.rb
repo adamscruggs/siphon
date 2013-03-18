813bab32d7a1a42c4d3c20e3f0f127375a00c30e
Adam Scruggs <ascruggs@gmail.com>
2013-03-18 07:46:51 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Siphon::Application.config.secret_token = '4d00fdd0eade2552540bd85ec2bf87ef1a5fc1531d906162210ce6c1ce2936e7f8fab7bfdf53be55c5e34dd8e9e7dd6d6e84c84324d2d314846a3bef20cc2265'
---
+++ siphon/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Siphon::Application.config.secret_token = 'c1c985b24693116d93dad408e1fe7d224028d160c87e1fa7356648b6e886e8dd509077a15f804d0e848050cf343f243eab79881d8bef5aba85ef7b4f4e9f87d4'
